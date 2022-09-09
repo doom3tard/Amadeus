8dd9b0e149fbf9b61ffe4ca3c4fd8e9dcdeca4be
doom3tard <113176833+doom3tard@users.noreply.github.com>
2022-09-09 16:27:19 +0530

    Update amadeus.py

--- amadeus.py
+++ amadeus.py
@@ -36,7 +36,7 @@ cache = None
 qcache = None
 chat_log = None
 botname = 'Makise Kurisu'
-username = 'Your name here'
+username = 'Rick'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
